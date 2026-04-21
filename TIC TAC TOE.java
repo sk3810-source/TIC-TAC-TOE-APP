5b007f67aa490a78bfaeffdb765447749fa7491e
sk3810-source <sk3810@srmist.edu.in>
2026-04-21 10:27:40 +0530

    Update TIC TAC TOE.java

--- TIC TAC TOE.java
+++ TIC TAC TOE.java
@@ -1,1 +1,23 @@
+public class TicTacToe {
+    public static void main(String[] args) {
 
+        // Step 1: Create a 3x3 board
+        char[][] board = new char[3][3];
+
+        // Step 2: Initialize board with '-'
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                board[i][j] = '-';
+            }
+        }
+
+        // Step 3: Display the board
+        System.out.println("Tic-Tac-Toe Board:");
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                System.out.print(board[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
